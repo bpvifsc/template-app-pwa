8568f6114ce6d4f0f697d618772a30c1a6eb8475
Bruno Panerai Velloso <bpv.ifsc@gmail.com>
2021-07-07 20:41:40 -0300

    adicionando path para funcionamento no github

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,30 @@
 const version = 3
 const cachename = 'app-cache-v'+version
+
+/**
+ * Arquivos que serão salvos no cache para uso offline
+ * O caminho para os arquivos deve ser completo e sem o dominio
+ * Ex: arquivo logo.png
+ *      URL: https://bpvifsc.github.io/template-app-pwa/imagens/logo.png
+ *      Caminho: /template-app-pwa/imagens/logo.png
+ *     
+ */
 const arquivos = [
-    "/",
-    "/index.html",
-    "/main.js",
-    "/service-worker.js",
-    "/manifest.json",
-    "/estilos.css",
-    "/imagens/bootstrap.jpg",
-    "/imagens/ig-logo.svg",
-    "/imagens/linus-torvalds.jpg",
-    "/imagens/logo.png",
-    "/imagens/mail.svg",
-    "/imagens/tim-berners-lee.jpg",
-    "/imagens/wpp-logo.svg",
-    "/imagens/icone192.png",
-    "/imagens/icone512.png"
+    "/template-app-pwa/",
+    "/template-app-pwa/index.html",
+    "/template-app-pwa/main.js",
+    "/template-app-pwa/service-worker.js",
+    "/template-app-pwa/manifest.json",
+    "/template-app-pwa/estilos.css",
+    "/template-app-pwa/imagens/bootstrap.jpg",
+    "/template-app-pwa/imagens/ig-logo.svg",
+    "/template-app-pwa/imagens/linus-torvalds.jpg",
+    "/template-app-pwa/imagens/logo.png",
+    "/template-app-pwa/imagens/mail.svg",
+    "/template-app-pwa/imagens/tim-berners-lee.jpg",
+    "/template-app-pwa/imagens/wpp-logo.svg",
+    "/template-app-pwa/imagens/icone192.png",
+    "/template-app-pwa/imagens/icone512.png"
   ]
   self.addEventListener('install', function(event) {
     event.waitUntil(

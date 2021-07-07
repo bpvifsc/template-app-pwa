9cafe00f435498a53a05404a9034916fa26dc086
Bruno Panerai Velloso <bpv.ifsc@gmail.com>
2021-07-07 20:03:19 -0300

    ajustes no service worker

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,43 @@
 const version = 1
-
+const cachename = 'app-cache-v'+version
 const arquivos = [
     "/",
-    "index.html",
-    "main.js",
-    "service-worker.js",
-    "manifest.json",
-    "estilo.css",
-    "imagens/bootstrap.jpg",
-    "imagens/iq-logo.svg",
-    "imagens/linus-torvalds.jpg",
-    "imagens/logo.png",
-    "imagens/mail.svg",
-    "imagens/tim-berners-lee.jpg",
-    "imagens/wpp-logo.svg"
+    "/index.html",
+    "/main.js",
+    "/service-worker.js",
+    "/manifest.json",
+    "/estilo.css",
+    "/imagens/bootstrap.jpg",
+    "/imagens/iq-logo.svg",
+    "/imagens/linus-torvalds.jpg",
+    "/imagens/logo.png",
+    "/imagens/mail.svg",
+    "/imagens/tim-berners-lee.jpg",
+    "/imagens/wpp-logo.svg"
   ]
-
-self.addEventListener("activate", function(){
-    caches.open("app-arquivos-"+version).then(cache => {
-      cache.addAll(arquivos)
-        .then(()=>{
-          caches.delete('app-arquivos-'+(version - 1))
-          caches.delete('app-arquivos')
-        })
-    })
-  })
-
-self.addEventListener("fetch", (event) => {
-    const pedido = event.request;
-    const promiseResponse = caches
-      .match(pedido)
-      .then((respostaCache) => {
-        return respostaCache ? respostaCache : fetch(pedido);
-      });
+  self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(cachename).then(function(cache) {
+        return cache.addAll(arquivos);
+      })
+    );
+  });
   
-    event.respondWith(promiseResponse);
-  }); 
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request).then(function(response) {
+      if (response !== undefined) {
+        return response;
+      } else {
+        return fetch(event.request).then(function (response) {
+          let responseClone = response.clone();
+          
+          caches.open(cachename).then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        }).catch(function () {
+          return caches.match('/index.html');
+        });
+      }
+    }));
+  });

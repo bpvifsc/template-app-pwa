9240e9a3504b92f79ea4abab46120e1c6722dd87
Bruno Panerai Velloso <bpv.ifsc@gmail.com>
2021-07-07 21:34:02 -0300

    botao instalar app

--- main.js
+++ main.js
@@ -5,16 +5,21 @@ function mostraPagina(pg) {
     pgAtual = pg;
 }
 
-//Registra o serviceWorker da aplicação
+//Registra o serviceWorker da aplicação para cache de recursos offline
 if ('serviceWorker' in navigator) {  
     navigator.serviceWorker.register("./service-worker.js");
 }
 
+//Verifica se o app pode ser instalado e mostra o botão
 var pedidoInstalacao;
-window.addEventListener('beforeinstallprompt', function(e) {
-  pedidoInstalacao = e;
+window.addEventListener('beforeinstallprompt', function(installPrompt) {
+    if(installPrompt){
+        $("#installAppBt").show();
+        pedidoInstalacao = installPrompt;
+    }
 });
 
+//Inicia a instalação do app
 function installApp() {
     pedidoInstalacao.prompt(); 
 }

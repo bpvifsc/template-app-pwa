2a5ef61968d9dd1b71f447926216ccce70ec9057
Bruno Panerai Velloso <bpv.ifsc@gmail.com>
2021-07-08 14:20:19 -0300

    ajuste na função de controle de fluxo da spa

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var pgAtual = "home";
+var pgAtual = "#home";
 function mostraPagina(pg) {
-    $("#"+pgAtual).hide();
-    $("#"+pg).show();
+    $(pgAtual).hide();
+    $(pg).show();
     pgAtual = pg;
 }
 
